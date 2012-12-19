3106647ac3cbdc2a86bd6520ffe7775ebf1d469f
Fiver <Fiver@21cn.com>
2012-12-19 21:52:14 +0800

    Disable sScriptMgr->OnPlayerLogin(pCurrChar), cause it freeze server, i dont know why
    Signed-off-by: Fiver <Fiver@21cn.com>

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1090,7 +1090,7 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder* holder)
 
     m_playerLoading = false;
 
-    sScriptMgr->OnPlayerLogin(pCurrChar);
+    //sScriptMgr->OnPlayerLogin(pCurrChar);  Cause crash....
     delete holder;
 }
 
